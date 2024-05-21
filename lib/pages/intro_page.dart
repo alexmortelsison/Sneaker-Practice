2241b4d116099f57b3e0b61c4bf8a9f718299d93
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-05-21 21:00:35 +0900

    updated UI

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,60 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:sneaker_practice/constants.dart';
-
-import 'home_page.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: kBackGroundColor,
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: Image.asset(
-                'images/logo.png',
-                height: 120,
-              ),
-            ),
-            const SizedBox(height: 20),
-            const Text('Just Do It'),
-            const SizedBox(height: 150),
-            Text(
-              'One stop shop for premium quality shoes',
-              style: TextStyle(color: Colors.grey[600]),
-            ),
-            const SizedBox(height: 80),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return const HomePage();
-                  }));
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.circular(12)),
-                  child: const Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: Center(
-                      child: Text(
-                        'Shop Now',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+    return const Scaffold();
   }
 }
