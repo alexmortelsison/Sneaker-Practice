19fead4baa1093a912620639692539652e45ebe4
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-05-22 10:22:11 +0900

    fixed bug

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:sneaker_practice/components/shoe_tile.dart';
+import 'package:sneaker_practice/models/shoe.dart';
 
-class ShopPage extends StatelessWidget {
-  const ShopPage({super.key});
+class ShopPage extends StatefulWidget {
+  const ShopPage({
+    super.key,
+  });
 
+  @override
+  State<ShopPage> createState() => _ShopPageState();
+}
+
+class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -22,7 +31,39 @@ class ShopPage extends StatelessWidget {
               ),
             ),
           ),
-          const Text('Hot Picks 🔥')
+          const SizedBox(height: 40),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Hot Picks 🔥',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+              ),
+              Text(
+                'See all',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ],
+          ),
+          const SizedBox(height: 40),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 4,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                Shoe shoe = Shoe(
+                    name: '1',
+                    price: '170',
+                    imagePath: 'images/1.png',
+                    description: 'Cool shoe');
+                return ShoeTile(
+                  shoe: shoe,
+                  onTap: () {},
+                );
+              },
+            ),
+          ),
+          const Divider(height: 40),
         ],
       ),
     );
