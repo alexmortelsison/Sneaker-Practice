6d7cfe414917608d75811fffc78b209cf8a7958b
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-05-22 09:45:18 +0900

    updated UI

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,10 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:sneaker_practice/constants.dart';
+
+import 'home_page.dart';
 
 class IntroPage extends StatelessWidget {
-  const IntroPage({super.key});
+  const IntroPage({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: kBackGroundColor,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Image.asset(
+                  'images/logo.png',
+                  width: 250,
+                ),
+              ),
+              Text(
+                'Just Do It',
+                style: TextStyle(
+                  color: Colors.grey[700],
+                ),
+              ),
+              const SizedBox(height: 50),
+              Text(
+                'Home of premium quality shoes',
+                style: TextStyle(
+                  color: Colors.grey[600],
+                ),
+              ),
+              const SizedBox(height: 80),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const HomePage(),
+                    ),
+                  );
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Center(
+                      child: Text(
+                        'Shop Now',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/pages/shop_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ShopPage extends StatelessWidget {
+  const ShopPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [Text('Search'), Icon(Icons.search)],
+              ),
+            ),
+          ),
+          const Text('Hot Picks 🔥')
+        ],
+      ),
+    );
+  }
+}
