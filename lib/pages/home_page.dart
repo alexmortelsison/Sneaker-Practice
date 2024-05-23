09864f62aff1c653b1683426eaa0cabc62b51269
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-05-23 16:20:49 +0900

    updated drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,21 +16,52 @@ class _HomePageState extends State<HomePage> {
       drawer: Drawer(
         backgroundColor: Colors.grey[900],
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            DrawerHeader(
-              child: Image.asset(
-                'images/logo.png',
-                color: Colors.white,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Column(
+                children: [
+                  DrawerHeader(
+                    child: Image.asset(
+                      'images/logo.png',
+                      color: Colors.white,
+                    ),
+                  ),
+                  const ListTile(
+                    leading: Icon(
+                      Icons.home,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      'Home',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  const ListTile(
+                    leading: Icon(
+                      Icons.info,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      'About',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
               ),
             ),
-            const ListTile(
-              leading: Icon(
-                Icons.home,
-                color: Colors.white,
-              ),
-              title: Text(
-                'Home',
-                style: TextStyle(color: Colors.white),
+            const Padding(
+              padding: EdgeInsets.only(left: 25, bottom: 25),
+              child: ListTile(
+                leading: Icon(
+                  Icons.logout,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  'Log Out',
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
             ),
           ],
