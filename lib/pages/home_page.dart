6ab473e2c145fb9fb1f06d16eb86495b5ab7e770
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-05-24 07:47:42 +0900

    updated UI

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sneaker_practice/constants.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,47 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: kBackGroundColor,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[900],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  DrawerHeader(
+                    child: Image.asset(
+                      'images/logo.png',
+                      color: Colors.white,
+                    ),
+                  ),
+                  const ListTile(
+                    leading: Icon(Icons.home, color: Colors.white),
+                    title: Text('Home', style: TextStyle(color: Colors.white)),
+                  ),
+                  const ListTile(
+                    leading: Icon(Icons.info, color: Colors.white),
+                    title: Text('About', style: TextStyle(color: Colors.white)),
+                  ),
+                ],
+              ),
+              const Padding(
+                padding: EdgeInsets.only(bottom: 25),
+                child: ListTile(
+                  leading: Icon(Icons.exit_to_app, color: Colors.white),
+                  title: Text('Log Out', style: TextStyle(color: Colors.white)),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
